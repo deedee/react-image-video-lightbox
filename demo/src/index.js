e584b31fa3a87bb48bf81d1222be3381d92f8a23
Deddy Syefria (deedee) <dsyefri@gmail.com>
2023-05-22 13:52:35 +0700

    geoportal

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,83 +1,38 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import axios from 'axios';
 
-import ReactImageVideoLightbox from "../../src";
+import ReactImageVideoLightbox from '../../src';
 
-export default class Demo extends Component {
+let data;
+
+export default class AttachmentViewer extends Component {
   state = {
-    lightboxOpen: false,
-    data: [
-      {
-        url: "https://placekitten.com/450/300",
-        type: "photo",
-        altTag: "some image",
-        tanggal: "2010-09-09"
-      },
-      {
-        url: "https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4",
-        type: "video",
-        title: "My Placeholder video",
-        tanggal: "2010-09-10"
-      },
-      {
-        url: "https://placekitten.com/550/500",
-        type: "photo",
-        altTag: "some other image",
-        tanggal: "2010-09-11"
-      },
-      {
-        url: "https://www.youtube.com/embed/ScMzIvxBSi4",
-        type: "video",
-        title: "some other video",
-        tanggal: "2010-09-12"
-      },
-    ]
+    lightboxOpen: true,
+    data: data
   };
 
   render() {
     return (
-      <div
-        style={{
-          fontFamily: "sans-serif",
-          margin: "auto",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <h1 style={{ textAlign: "center" }}>
-          React image &amp; video lightbox demo
-        </h1>
-        <h3 style={{ textAlign: "center" }}>
-          Click the button to view the lightbox
-        </h3>
-        <div>
-          <button
-            style={{
-              padding: 20,
-              border: "none",
-              borderRadius: 5,
-              backgroundColor: "lightgrey",
-            }}
-            onClick={() => this.setState({ lightboxOpen: true })}
-          >
-            Open lightbox
-          </button>
-        </div>
-        {this.state.lightboxOpen && (
           <ReactImageVideoLightbox
             data={this.state.data}
             startIndex={0}
             showResourceCount={true}
-            onCloseCallback={() => this.setState({ lightboxOpen: false })}
+           // onCloseCallback={() => this.setState({ lightboxOpen: false })}
             onNavigationCallback={(currentIndex) =>
               console.log(`Current index: ${currentIndex}`)
             }
           />
-        )}
-      </div>
     );
   }
 }
 
-render(<Demo />, document.querySelector("#demo"));
+const params = new URLSearchParams(window.location.search);
+const t = params.get('t');
+const n = params.get('n');
+const i = params.get('i');
+axios.get(`/geokincia/att/detail/${t}/${n}/${i}`)
+.then(result => {
+  data = result.data;
+  render(<AttachmentViewer />, document.querySelector("#demo"));
+});
